9280bdd1cae38363186c50f628763ca8b0a9a194
Youri T. K. K. Mattar <youri@youhide.com.br>
2018-03-14 10:16:17 -0300

    vote func
    obs: database on dev

--- functions/vote/vote.js
+++ functions/vote/vote.js
@@ -3,10 +3,26 @@
 // MySQL.create:  INSERT INTO `gceval` (`postid`, `stars`, `ip`, `createdAt`)
 //                 values (1651, 5, '000.000.000.000', '2018-03-13 19:26:08')
 
+// new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"})
+// new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+const utils = require('../../includes/utils.js');
+const config = require('../../includes/config.json');
+const mysql = require('../../node_modules/mysql');
+
+let pool = mysql.createPool(config.database);
+
 module.exports.vote = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  var utils = require("../../includes/utils.js");
+  const body = JSON.parse(event.body);
 
-  callback(null, utils.resolve(event));
+  pool.getConnection(function(err, connection) {
+    connection.query("INSERT INTO `gceval` (`postid`, `stars`, `ip`, `createdAt`) values (" + body.postid + ", " + body.stars + ", '" + body.ip + "', CONVERT_TZ(NOW(),'SYSTEM','America/Sao_Paulo'))", function (error, results, fields) {
+      connection.release();
+      if (error) callback(error);
+      else callback(null, utils.resolve(results));
+    });
+  });
 
 };
--- includes/utils.js
+++ includes/utils.js
@@ -3,6 +3,9 @@
 module.exports.resolve = (input) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(input)
   };
 
